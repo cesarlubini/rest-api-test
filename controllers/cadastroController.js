5a6db2b1d7f8ed6aea17c531e5c4049448ed19a6
cesarlubini <cesar.lubini@gmail.com>
2020-11-10 21:11:03 -0300

    create api route UPDATE, DELETE and GET BY ID

--- controllers/cadastroController.js
+++ controllers/cadastroController.js
@@ -3,13 +3,32 @@ const Cadastro = require('../models/cadastroModel');
 module.exports = app => {
 
   app.get('/cadastros', (req,res) => {
-    Cadastro.lista(res);
+    Cadastro.listaTodos(res);
   });
+
+  app.get('/cadastro/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+
+    Cadastro.cadastro(id, res);
+  })
 
   app.post('/cadastro', (req, res) => {
     
     const cadastro = req.body;
     Cadastro.novo(cadastro, res);
+  })
+
+  app.put('/cadastro/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const cadastro = req.body;
+
+    Cadastro.atualiza(id, cadastro, res);
+  })
+
+  app.delete('/cadastro/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    Cadastro.deleta(id, res);
   })
 
 }
--- models/cadastroModel.js
+++ models/cadastroModel.js
@@ -2,11 +2,32 @@ const Database = require('../database/db');
 
 class Cadastro {
 
-  async lista(res) {
-    const db = await Database
-    const results = await db.all("SELECT * FROM Cadastros")
-    res.send(results);
-    console.log(results);
+  async listaTodos(res) {
+    try{
+      const db = await Database
+      const results = await db.all("SELECT * FROM Cadastros")
+      return res.send(results);
+
+    } catch(error) {
+      console.log(error)
+      return res.send("Erro ao incluir Cadastro")
+    }
+  }
+
+  async cadastro(id, res) {
+    try {
+      const db = await Database;
+      const result = await db.get(`
+        SELECT * FROM Cadastros WHERE id=${id}
+      `)
+
+      return res.send(result);
+
+    } catch(error) {
+      console.log(error);
+
+      return res.send("Erro ao buscar cadastro");
+    }
   }
 
   async novo(cadastro, res) {
@@ -36,6 +57,46 @@ class Cadastro {
       return res.send("Erro no banco de dados")
     }
   }
+  
+  async atualiza(id, cadastro, res) {
+    try {
+      const db = await Database;
+      await db.run(`
+        UPDATE Cadastros SET
+          name = "${cadastro.name}",
+          age = "${cadastro.age}",
+          gender = "${cadastro.gender}",
+          smoke = "${cadastro.smoke}",
+          drink = "${cadastro.drink}"
+        WHERE id=${id}
+      `)
+
+      return res.send(cadastro)
+
+    } catch(error) {
+      console.log(error)
+
+      return res.send("Erro ao atualizar dados")
+    }
+  }
+
+  async deleta(id, res) {
+    try{
+      const db = await Database;
+      await db.run(`
+        DELETE FROM Cadastros WHERE id=${id}
+      `)
+  
+      return res.send(`ID ${id} deletado com sucesso`);
+
+    } catch(error) {
+
+    console.log(error)
+    return res.send("Erro ao deletar dados")
+
+    }
+  }
 }
+
 
 module.exports = new Cadastro;
